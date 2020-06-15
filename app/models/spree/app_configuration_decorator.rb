3c1c16bd1b9e5df6eb6630bcba55087b7e0f79d3
arkerrig <arkerrig@gmail.com>
2020-06-15 10:19:16 -0700

    update appconfigdecorator-3

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,5 +1,8 @@
 require 'spree/core'
-Spree::AppConfigurationDecorator.class_eval do
+config = Rails.application.config
+
+config.after_initialization do
+Spree::AppConfiguration.class_eval do
     preference :mails_from, :string, default: 'spree@example.com'
     preference :enable_mail_delivery, :boolean, default: true
     preference :mail_bcc, :string, default: 'spree@example.com'
@@ -13,4 +16,5 @@ Spree::AppConfigurationDecorator.class_eval do
     preference :mail_auth_type, :string#, default: Core::MailSettings::MAIL_AUTH[0]
     preference :smtp_username, :string, default: nil
     preference :smtp_password, :string, default: nil
+end
 end
